dcb340b698dd1db95ed4e64cad26efdb0c6259e5
Varga Adam <ed1lord1@gmail.com>
2017-03-31 04:44:24 -0700

    Added for conflict.

--- src/main/java/hu/bme/mit/spaceship/SpaceShip.java
+++ src/main/java/hu/bme/mit/spaceship/SpaceShip.java
@@ -5,7 +5,7 @@ package hu.bme.mit.spaceship;
 * (collects just the most important ones currently)
 */
 
-/*Modified for conflict v1*/
+/*Modified for conflict v2*/
 public interface SpaceShip {
 
   /**
